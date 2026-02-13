302d5009b7410fa26d546feda52efd6f3d1dcb16
Erley <eblanco@tuworker.com>
2026-02-13 09:40:20 -0500

    bump version to 0.1.1 and update gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sonika-ai-toolkit",
-    version="0.1.0",
+    version="0.1.1",
     description="Toolkit para creación de agentes de IA y procesamiento de documentos",
     author="Erley Blanco Carvajal",
     license="MIT License",
